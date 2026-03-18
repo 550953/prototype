3a4f9b2b60df63ebab6eed669e86a21a0ecb4a0c
Shikin Nikolay <15770740+550953@users.noreply.github.com>
2026-03-18 23:12:04 +0300

    Delete sber.sh

--- sber.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-if [ -d /etc/ssh/sshd_config.d ]; then
-    sudo grep -rl "PasswordAuthentication no" /etc/ssh/sshd_config.d/ 2>/dev/null | xargs -r sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/'
-fi
-echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
-sudo systemctl restart ssh
-sudo sshd -T | grep passwordauthentication
